c68f6fbad18a730934e87806fe11395635acc432
Shawn Kuruganti <skuruganti45@gmail.com>
2018-06-07 01:19:46 -0400

    switch this to a js file for node

--- First_Js_Work/Bottles of beer.js
+++ First_Js_Work/Bottles of beer.js
@@ -1,5 +1,5 @@
 for (let i = 99 ; i >= 0 ; i--) {
-  
+
   if (i > 0) {
     console.log(i + " bottles of beer on the wall, " + i + " bottles of beer, take one down, pass it around " + (i-1) + " bottles of beer!");
   }
